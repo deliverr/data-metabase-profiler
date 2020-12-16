37a9bc07d7f056b9cdb701f68f689383a141c2f3
Emmet Murphy <emmet@emurphy.net>
2020-12-16 15:18:38 +0900

    no need for runs when list report tables

--- metabase/list_report_tables.py
+++ metabase/list_report_tables.py
@@ -2,7 +2,6 @@
 Lists Metabase report cards, and the tables the queries reference
 """
 import json
-import pandas as pd
 import sql_metadata
 from metabase import get_all_cards, get_card_results_pandas, get_table
 
@@ -10,7 +9,7 @@ from metabase import get_all_cards, get_card_results_pandas, get_table
 def main():
     report_cards = get_all_cards()
     active_cards = get_card_results_pandas(6942, params={ 'days_back': 31 })
-    card_ids_to_runs = pd.Series(active_cards.runs, index=active_cards.card_id).to_dict()
+    active_card_ids = active_cards.card_id.tolist()
     print(f"Retrieved {len(report_cards)} report cards")
 
     reports = []
@@ -18,13 +17,12 @@ def main():
         if report_card['dataset_query'] is None:
             print(f"skipping {json.dumps(report_card)}")
             continue
-        if report_card['id'] not in card_ids_to_runs:
+        if report_card['id'] not in active_card_ids:
             print(f"card {report_card['name']} ({report_card['id']}) is not active, skipping")
             continue
         report_tables = {
             "id": report_card['id'],
             "name": report_card['name'],
-            "runs": card_ids_to_runs[report_card['id']]
         }
         reports.append(report_tables)
         query_type = report_card['dataset_query']['type']
